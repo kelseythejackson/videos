5b99d6aea24f6f9a26e28ebada3d41d87a0feb68
Kelsey Jackson <kelseythej@gmail.com>
2022-12-11 01:30:11 -0600

    finished app

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,12 +1,16 @@
+import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   const {
-    snippet: { description, thumbnails },
+    snippet: { description, thumbnails, title },
   } = video;
   return (
-    <div>
-      <img alt={description} src={thumbnails.medium.url} />
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img className="ui image" alt={description} src={thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{title}</div>
+      </div>
     </div>
   );
 };
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
-  const renderedList = videos.map((video) => <VideoItem key={video.id.videoId} video={video} />);
-  return <div>{renderedList}</div>;
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
+    return <VideoItem onVideoSelect={onVideoSelect} key={video.id.videoId} video={video} />;
+  });
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
